056fdc9d324ce3b82cf95e9a969a1cbf6d31c8ad
Joshulok <joshulok@icloud.com>
2024-05-21 21:14:29 +0200

    changes completed

---
+++ main.js
@@ -0,0 +1,13 @@
+let userAge = 29
+// Print in the console each odd number in the life of Joshua
+function printOddAges() {
+
+    for ( let i = 0; i <= userAge; i++) {
+
+        if (i%2 == 0) {
+          console.log(i)
+        }
+    }
+}
+
+printOddAges()
